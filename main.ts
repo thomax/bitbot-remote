9d303387da632a253a32f5979dcdfc82cbe07e1a
Thomas Drevon <thomas@drevon.no>
2022-11-29 19:26:32 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("left")
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("forward")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("right")
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendString("stop")
+})
+radio.setGroup(55)
